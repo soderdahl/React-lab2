4b78c6614ce4e42bb84c7cfb2b53e99b2c75f740
Nutcha Romjun <soderdahl.nutcha@gmail.com>
2019-04-25 22:10:56 +0200

    Initial commit

---
+++ src/components/ui/DisplayBooks/DisplayList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+class DisplayList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      books: [],
+      title: "",
+      author: ""
+    }
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+
+    )
+  }
+}
+
+
+export default DisplayList;
